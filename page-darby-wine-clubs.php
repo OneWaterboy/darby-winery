bbb0960edd8c1da69d899eacddb6c3ff162c6288
OneWaterboy <abrahmrollins@gmail.com>
2023-06-24 23:04:47 -0700

    Styled out the the wine club cards and made adjustments to the layouts

--- page-darby-wine-clubs.php
+++ page-darby-wine-clubs.php
@@ -26,32 +26,38 @@ get_header(); ?>
 <?php endif ?>
 
 	<div id="primary" <?php astra_primary_class(); ?>>
-    <?php
-        if( have_rows('wine_clubs' , 'option') ):
-            while( have_rows('wine_clubs' , 'option') ): the_row(); ?>
-                <div class="club-cards">
-                    <div class="club-card-title">
-                        <p class="club-name"><?php echo get_sub_field('wine_club_name'); ?></p>
-                    </div>
-                    <div class="club-card-details">
-                        <div class="wine-club-details">
-                            <?php
-                                if( have_rows('wine_club_benefits') ):
-                                    while( have_rows('wine_club_benefits') ): the_row(); ?>
-                                        <span class="club-benefit"><?php echo get_sub_field('wine_club_benefit_item'); ?></span>
-                                    <?php endwhile; ?>
-                            <?php endif; ?>
+        <p class="wine-clubs-header">Our club wines produced just for our beloved members!I am always shocked that other wineries offer wines anyone can go to a store and purchase. Doesn’t make sense and we won’t do it. We hand pick our best barrels and create small lots of blends and single vineyard offerings, just for you.</p>
+        <div class="club-cards">
+            <?php
+                if( have_rows('wine_clubs' , 'option') ):
+                    while( have_rows('wine_clubs' , 'option') ): the_row(); ?>
+                        <div class="club-card">
+                            <div class="club-card-title">
+                                <p class="club-name"><?php echo get_sub_field('wine_club_name'); ?></p>
+                                <div class="club-price">
+                                    $<?php echo get_sub_field('wine_club_price'); ?> Yearly
+                                </div>
+                            </div>
+                            <div class="club-card-details">
+                                <div class="wine-club-details">
+                                    <?php
+                                        if( have_rows('wine_club_benefits') ):
+                                            while( have_rows('wine_club_benefits') ): the_row(); ?>
+                                                <div class="benefit-line-items">
+                                                    <img src="/wp-content/themes/darby-winery/graphics/wine-bottle.png'; ?>" />
+                                                    <span class="club-benefit"><?php echo get_sub_field('wine_club_benefit_item'); ?></span>
+                                                </div>
+                                            <?php endwhile; ?>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <div class="club-sign-up">
+                                <a target="_blank" href="<?php echo get_sub_field('wine_club_sign_up_link'); ?>" class="sign-up-button">Join the club</a>
+                            </div>
                         </div>
-                        <div class="wine-club-image">
-                            <img src="<?php echo get_sub_field('wine_club_image'); ?>" />
-                        </div>
-                    </div>
-                    <div class="club-sign-up">
-                        <a target="_blank" href="<?php echo get_sub_field('wine_club_sign_up_link'); ?>" class="sign-up-button"></a>
-                    </div>
-                </div>
-            <?php endwhile; ?>
-        <?php endif; ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
 
 	</div><!-- #primary -->
 
